7203e75351c056014b7866d44151c8b162acff06
Nicolás Riquelme <nrriquelme@gmail.com>
2018-01-04 23:43:18 -0300

    añadiendo partials

--- generador_pruebas/main.go
+++ generador_pruebas/main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -9,15 +10,24 @@ import (
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseFiles("templates/index.html"))
+	tpl = template.Must(template.ParseFiles("templates/index.html", "templates/partial/header.html", "templates/partials/footer.html", "templates/alternativas.html"))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	tpl.ExecuteTemplate(w, "index.html", nil)
 }
 
+func next(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Funcion next")
+	http.Redirect(w, r, "/test2", 301)
+}
+
 func main() {
 	http.HandleFunc("/", index)
+	// ruta dummy para probar pegarle con ajax
+	http.HandleFunc("/siguiente", next)
+
+	// Servir los assets estaticos
 	fs := http.FileServer(http.Dir("assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	http.ListenAndServe(":8080", nil)
